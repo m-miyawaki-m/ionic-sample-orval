c7d3a4ccdc86eb22f87d9a721c0910f612b78abb
m-miyawaki <miyawakifamilymyam@gmail.com>
2026-05-08 00:11:47 +0900

    feat(frontend): type VITE_USE_MOCK and VITE_API_BASE_URL

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_USE_MOCK: 'msw' | 'none' | undefined
+  readonly VITE_API_BASE_URL: string | undefined
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
