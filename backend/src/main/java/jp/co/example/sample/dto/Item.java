5c690116f2ce59e131f9b265678b10c2946165fe
m-miyawaki <miyawakifamilymyam@gmail.com>
2026-05-08 00:25:40 +0900

    feat(backend): add Item and ItemCreate DTOs

---
+++ backend/src/main/java/jp/co/example/sample/dto/Item.java
@@ -0,0 +1,3 @@
+package jp.co.example.sample.dto;
+
+public record Item(Long id, String name, Integer price) {}
---
+++ backend/src/main/java/jp/co/example/sample/dto/ItemCreate.java
@@ -0,0 +1,3 @@
+package jp.co.example.sample.dto;
+
+public record ItemCreate(String name, Integer price) {}
